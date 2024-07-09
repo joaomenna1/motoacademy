819917ad8c0e07a4d96f5de568534219e31ce87b
joaomenna1 <joaomenna1@gmail.com>
2024-07-08 20:18:45 -0400

    add javascript

---
+++ javascript/aula.js
@@ -0,0 +1,24 @@
+const number = 1  //number
+const string = "texto" // string
+const booleano = true // true ou falso
+const nulo = null 
+const undefined = undefined;
+
+const objeto = {
+    nome: "euclides",
+    idade: 22
+}
+
+objeto.nome = "Linton"
+
+
+const array = ["teste", 2121321, true]
+
+const soma = 1 + 1
+
+const subtraçao = 2-1
+
+const multiplicação = 2*1
+
+const divisao = 4/2
+
